68911aa25acdd606118fb8ad03c4a3f82c5e3c05
unniiimon <unniiimon123@gmail.com>
2025-05-26 14:59:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_URI = os.getenv("MONGO_URI")
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "1426582599").split()))
-
+MONGO_URL = os.getenv("MONGO_URL")
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split()))
+FILES_CHANNEL = os.getenv("FILES_CHANNEL")  # optional channel for file input
